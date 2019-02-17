d0a98b1c8883c3e3ea76594c1ca1821b6d8ac9a1
softsprocket <greg@softsprocket.com>
2019-02-17 10:01:18 -0800

    fix typo

--- touch.go
+++ touch.go
@@ -8,7 +8,7 @@ import (
 
 func redirectToHttps(w http.ResponseWriter, r *http.Request) {
 	// Redirect the incoming HTTP request. Note that "127.0.0.1:8081" will only work if you are accessing the server from your local machine.
-	http.Redirect(w, r, "https://127.0.0.1:8081"+r.RequestURI, http.StatusMovedPermanently)
+	http.Redirect(w, r, "http"+r.RequestURI, http.StatusMovedPermanently)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +23,5 @@ func main() {
 	// Start the HTTPS server in a goroutine
 	go http.ListenAndServeTLS(":https", cert, key, nil)
 	// Start the HTTP server and redirect all incoming connections to HTTPS
-	http.ListenAndServe(":http", http.HandlerFunc(redirectToHttps))
+	http.ListenAndServe(":http", http.HandlerFunc(nil))
 }
